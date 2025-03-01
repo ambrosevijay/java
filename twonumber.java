c32cf13192079d4a38b27fb03c430f10dbc91441
ambrosevijay <ambrosevijay91@gmail.com>
2025-03-01 09:03:44 -0800

    first commit

---
+++ twonumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class twonumber
+{
+    public static void main(String[] args) {
+        Scanner user=new Scanner(System.in);
+        System.out.println("Enter the number1:");
+        int num1=user.nextInt();
+        System.out.println("Enter the number2");
+        int num2=user.nextInt();
+        if(num1>num2)
+        {
+            System.out.println("number one is biggest!");
+        }else
+        {
+            System.out.println("number two is biggest!");
+        }
+
+    }
+}
