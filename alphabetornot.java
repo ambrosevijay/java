490d650969862ccecce1547aeacf0a68178bc248
Vijayan_Narasimman <65540422+ambrosevijay@users.noreply.github.com>
2025-03-02 02:19:08 -0800

    Update alphabetornot.java

--- alphabetornot.java
+++ alphabetornot.java
@@ -6,9 +6,9 @@ public class AlphabetorNot {
 
 	public static void main(String[] args) {
 		// find alphabets or not
-		Scanner in=new Scanner(System.in);
+		Scanner user=new Scanner(System.in);
 		System.out.println("Enter the values:");
-		char ch=in.next().charAt(0);
+		char ch=user.next().charAt(0);
 		if(ch>='a' && ch<='z' || ch>='A' && ch<='Z')
 		{
 			System.out.println("Alphabets here!");
